fe71d33bc7b59caee63b74bc14d739bce853bb70
Ignacio <josenunezm2001@gmail.com>
2025-11-30 13:31:32 -0300

    Conexión a la base de datos

---
+++ src/tests/db_connection.test.js
@@ -0,0 +1,12 @@
+import pool from "../config/config.js";
+
+async function testConnection() {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        console.log("Connect to DB »", result.rows[0]);
+    } catch (error) {
+        console.error("Error to connect to DB »", error);
+    }
+}
+
+testConnection();
